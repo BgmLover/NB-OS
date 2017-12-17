36596ee40155cfba00f37cbb72d09603828b07df
BgmLover <787782453@qq.com>
2017-12-17 23:18:42 +0800

    删去之前的内容

--- kernel/syscall/syscall12.h
+++
@@ -1,6 +0,0 @@
-#ifndef _SYSCALL12_H
-#define _SYSCALL12_H
-
-void syscall12(unsigned int a0, unsigned int a1, unsigned int a2, unsigned int a3);
-
-#endif  // ! _SYSCALL12_H
--- kernel/syscall/syscall4.c
+++
@@ -1,7 +0,0 @@
-
-#include <arch.h>
-#include <zjunix/syscall.h>
-
-void syscall4(unsigned int a0, unsigned int a1, unsigned int a2, unsigned int a3) {
-    *GPIO_LED = a0;
-}
