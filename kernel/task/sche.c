13858588b1f5bfe759c9360a84c5317df0807297
Jambobo <1065259087@qq.com>
2018-01-01 18:14:15 +0800

    进程调度

--- kernel/task/sche.c
+++ kernel/task/sche.c
@@ -24,6 +24,7 @@ list_pcb normal_list;
 list_pcb below_normal_list;
 list_pcb idle_list;
 list_pcb test;
+list_pcb *next_list;
 
 PCB* get_current_pcb()
 {
@@ -44,6 +45,30 @@ void print_2_fun()
     {
         kernel_printf(" 2 \n");
     }
+}
+
+void print_context(context* pt_context)
+{
+    kernel_printf("epc:  %x\n",pt_context->epc);
+    kernel_printf("at:   %x\n",pt_context->at);
+    kernel_printf("v0:  %x,  v1:  %x\n",pt_context->v0,pt_context->v1);
+    kernel_printf("a0:  %x,  a1:  %x",pt_context->a0,pt_context->a1);
+    kernel_printf("a2:  %x,  a3:  %x\n",pt_context->a2,pt_context->a3);
+    kernel_printf("t0:  %x,  t1:  %x",pt_context->t0,pt_context->t1);
+    kernel_printf("t2:  %x,  t3:  %x",pt_context->t2,pt_context->t3);
+    kernel_printf("t4:  %x,  t5:  %x",pt_context->t4,pt_context->t5);
+    kernel_printf("t6:  %x,  t7:  %x\n",pt_context->t6,pt_context->t7);
+    kernel_printf("s0:  %x,  s1:  %x",pt_context->s0,pt_context->s1);
+    kernel_printf("s2:  %x,  s3:  %x",pt_context->s2,pt_context->s3);
+    kernel_printf("s4:  %x,  s5:  %x",pt_context->s4,pt_context->s5);
+    kernel_printf("s6:  %x,  s7:  %x\n",pt_context->s6,pt_context->s7);
+    kernel_printf("t8:  %x,  t9:  %x\n",pt_context->t8,pt_context->t9);
+    kernel_printf("hi:  %x,  lo:  %x\n",pt_context->hi,pt_context->lo);
+    kernel_printf("gp:  %x\n",pt_context->gp);
+    kernel_printf("sp:  %x\n",pt_context->sp);
+    kernel_printf("fp:  %x\n",pt_context->fp);
+    kernel_printf("ra:  %x\n",pt_context->ra);
+ 
 }
 
 void test_sched()
@@ -95,7 +120,11 @@ void test_sched()
     //init->pcb.context=(context*)(init+PAGE_SIZE-(sizeof(context)));
     proc1->pcb.context=(context*)((unsigned int)proc1+sizeof(PCB));
     clean_context(proc1->pcb.context);
-    proc1->pcb.context->epc=(unsigned int)(&print_0_fun);
+    proc1->pcb.context->epc=(unsigned int)(print_0_fun);init->context->sp=(unsigned int)init+PAGE_SIZE;
+    proc1->context->sp=(unsigned int)proc1+PAGE_SIZE;
+    unsigned int init_gp;
+    asm volatile("la %0, _gp\n\t" : "=r"(init_gp));
+    proc1->context->gp=init_gp;
     proc1->pcb.asid=get_emptypid();
     if(proc1->pcb.asid<0){
         kernel_printf("failed to get right asid\n");   
@@ -134,8 +163,55 @@ void test_sched()
     proc1->pcb.state=STATE_RUNNING;
 
 
-    add_to_foreground_list(&(proc1->pcb.process));
-   // add_to_foreground_list(&(proc2->pcb.process));
+
+    proc2->pcb.context=(context*)((unsigned int)proc2+sizeof(PCB));
+    clean_context(proc2->pcb.context);
+    proc2->pcb.context->epc=(unsigned int)(print_0_fun);init->context->sp=(unsigned int)init+PAGE_SIZE;
+    proc2->context->sp=(unsigned int)proc2+PAGE_SIZE;
+    unsigned int init_gp;
+    asm volatile("la %0, _gp\n\t" : "=r"(init_gp));
+    proc2->context->gp=init_gp;
+    proc2->pcb.asid=get_emptypid();
+    if(proc2->pcb.asid<0){
+        kernel_printf("failed to get right asid\n");   
+        return;
+    }
+    proc1->pcb.pgd=(pgd_term*)kmalloc(PAGE_SIZE);//分配页目录空间
+    if(proc2->pcb.pgd==NULL)
+    {
+        kernel_printf("failed to kmalloc space for pgd\n");
+        return;
+    }
+    //初始化pgd每一项
+    int i=0;
+    for(i=0;i<PAGE_SIZE>>2;i++)
+    {
+        (proc2->pcb.pgd)[i]=0;
+    }
+    //设置pgd属性为默认属性——可写
+    kernel_strcpy(proc2->pcb.name, "print_0");
+    proc2->pcb.parent=0;//init没有父进程
+    proc2->pcb.uid=0;
+    proc2->pcb.counter=DEFAULT_TIMESLICES;
+    proc2->pcb.start_time=0;//get_time();
+    proc2->pcb.state=STATE_WAITTING;
+    proc2->pcb.priority=HIGH_PRIORITY;//设置优先级为最低优先级
+    proc2->pcb.policy=0;//暂未定义调度算法
+    proc2->pcb.shm=NULL; //shared memory
+
+    INIT_LIST_PCB(&proc2->pcb.sched,&(proc2->pcb));
+    INIT_LIST_PCB(&proc2->pcb.process,&(proc2->pcb));
+    //暂不考虑线程
+    proc2->pcb.thread_head=NULL;
+    proc2->pcb.num_thread=0;
+    
+    add_task(&(proc2->pcb.process));//添加到pcb链表中
+    proc2->pcb.state=STATE_RUNNING;
+
+
+    //add_to_foreground_list(&(proc1->pcb.process));
+    current=&(proc1->pcb);
+    add_to_foreground_list(&(proc2->pcb.process));
 }
 
 
@@ -159,17 +235,18 @@ void init_sched()
     //初始情况为前台队列进程
     flag=1;
     counter=FOREGROUNG_TIMESLICES;
-    //counter=BACKGROUND_TIMESLICES;
     
     kernel_printf("Timeslices complete!\n");
      
-    current=get_first_task(&pcbs);
-    insert_tail(current,&high_list);
+    //把init进程放在后台队列中
+    //current=get_first_task(&pcbs); 
+    add_to_background_list(get_first_task(&pcbs));
+    next_list=&high_list;
 
     test_sched();
     kernel_printf("Add process complete!\n");
     //register_syscall(10, pc_kill_syscall);
-    register_interrupt_handler(7, pc_schedule);
+    register_interrupt_handler(7, schedule);
 
     asm volatile(
         "li $v0, 100000000\n\t"
@@ -177,19 +254,32 @@ void init_sched()
         "mtc0 $zero, $9");
 }
 
-void pc_schedule(unsigned int status, unsigned int cause, context* pt_context) {
+void schedule(unsigned int status, unsigned int cause, context* pt_context) {
     // Save context
     list_pcb *pos;
     kernel_printf("copy context\n");
     copy_context(pt_context, current->pcb->context);
+   *GPIO_SEG =counter;
     sched();
 
-    copy_context(current->pcb->context, pt_context);
-    kernel_printf("load context\n");
-
+    //修改tlb中asid
+    unsigned int new_Entryhi=0;
+    new_Entryhi|=current->pcb->asid;
+    asm volatile(
+        "mtc0 %0,$10\n\t"
+        :"=r"(new_Entryhi));
+    
     kernel_printf("%x\n",pt_context->epc);
+    
+    copy_context(current->pcb->context, pt_context);
+    
+    kernel_printf("load context\n");
+    print_context(pt_context);
+
+    
     kernel_printf("%x\n",current->pcb->context->epc);
-    asm volatile("mtc0 $zero, $9\n\t");
+   asm volatile("mtc0 $zero, $9\n\t");
+    //kernel_printf("set time\n");
 }
 
 //初始化队列
@@ -216,10 +306,20 @@ list_pcb *get_first_task(list_pcb *head)
         return NULL;
 
     task=head->next;
+    //kernel_printf("head->next is %s\n",task->pcb->name);
     if(task->next==head)
+    {
+        kernel_printf("task->next=head\n");
+        //while(1);
+    }
     __list_pcb_del(head,task->next);
     if(head->next==head)
+    {
+        kernel_printf("head list is empty\n");
+        //while(1);
+    }
     
+
     init_pcb_list(task);
 
     return task;
@@ -255,23 +355,17 @@ void add_to_background_list(list_pcb *task)
 
 //后台进程调度
 unsigned int background_sched()
-{
+{//重新修改过，待调试
     kernel_printf("background sched begin\n");
     list_pcb *next;
     list_pcb *old;
-    if(!current)
-    {
-        current->pcb->state=STATE_READY;
-        current->pcb->counter=BACKGROUND_PER_TIMESLICES;
-        add_to_background_list(current);
-    }
 
     if(list_is_empty(&background_list))
     {
         kernel_printf("No process background\n");
         //切换到前台状态
-        counter=FOREGROUNG_TIMESLICES;
         flag=1;
+        counter=FOREGROUNG_TIMESLICES;
         kernel_printf("current state timeslices is %d\n",counter);
         kernel_printf("current state is %d\n",flag);
         goto background_sched_error;
@@ -303,80 +397,96 @@ background_sched_error:
 
 //前台进程调度
 unsigned int foreground_sched()
-{
+{//重新修改过，待调试
     kernel_printf("foreground sched begin\n");
 
     list_pcb *next;
     list_pcb *old;
     int lower_priority;
     int timeslices;
-    list_pcb *lower_list;
 
     if(!list_is_empty(&high_list))
     {
         kernel_printf("high list is not empty\n");
         next=get_first_task(&high_list);
-		//优先级高的进程允许在优先级最高的队列多运行一个时间片
-        if(current->pcb->priority==HIGH_PRIORITY)
-        {
-            timeslices=HIGH_TIMESLICES;
-            lower_list=&high_list;
-            list_pcb_add_tail(current,&high_list);
-        }
-        else 
-        {
-            timeslices=ABOVE_NORMAL_TIMESLICES;
-            lower_list=&above_normal_list;
-            list_pcb_add_tail(current,&above_normal_list);
-        }
+        // if(next->pcb->priority==HIGH_PRIORITY)
+        // {//如果优先级是最高，那么允许在最高优先级队列中执行两个之间片
+        //     next_list=&high_list;
+        //     timeslices=HIGH_TIMESLICES;
+        // }
+        // else
+        // {//否则按照正常处理
+        //     if(next!=current)
+        //     {//如果当前进程与下一个取到的进程不是同一个，那么正常处理
+        //         next_list=&above_normal_list;
+        //         timeslices=ABOVE_NORMAL_TIMESLICES;
+        //     }
+        //     else
+        //     {//如果队列中只有一个进程且为最高优先级，那么第二次取出该进程时做特殊处理
+        //         current=next;
+        //         next_list=&above_normal_list;
+        //         current->pcb->counter=HIGH_TIMESLICES;
+        //         current->pcb->priority=ABOVE_NORMAL_PRIORITY;
+        //         return 0;
+        //     }
+        // }
+        // lower_priority=ABOVE_NORMAL_PRIORITY;
+        timeslices=HIGH_TIMESLICES;
         lower_priority=ABOVE_NORMAL_PRIORITY;
+        next_list=&above_normal_list;
+        //kernel_printf("next process is %s\n",next->pcb->name);
     }else if(!list_is_empty(&above_normal_list))
     {
         kernel_printf("above normal list is not empty\n");
         next=get_first_task(&above_normal_list);
         lower_priority=NORMAL_PRIORITY;
-        timeslices=NORMAL_TIMESLICES;
-        list_pcb_add_tail(current,&normal_list);
+        timeslices=ABOVE_NORMAL_TIMESLICES;
+        next_list=&normal_list;
+       // kernel_printf("next process is %s\n",next->pcb->name);
+        //kernel_printf("next list contain %s\n",normal_list.next->pcb->name);
     }else if(!list_is_empty(&normal_list))
     {
         kernel_printf("normal list is not empty\n");
         next=get_first_task(&normal_list);
         lower_priority=BELOW_NORMAL_PRIORITY;
-        timeslices=BELOW_NORMAL_TIMESLICES;
-        lower_list=&below_normal_list;
-        list_pcb_add_tail(current,&below_normal_list);
+        timeslices=NORMAL_TIMESLICES;
+        next_list=&below_normal_list;
     }else if(!list_is_empty(&below_normal_list))
     {
         kernel_printf("below normal list is not empty\n");
         next=get_first_task(&below_normal_list);
         lower_priority=IDLE_PRIORITY;
-        timeslices=IDLE_TIMESLICES;
-        lower_list=&idle_list;
-        list_pcb_add_tail(current,&idle_list);
+        timeslices=BELOW_NORMAL_TIMESLICES;
+        next_list=&idle_list;
     }else if(!list_is_empty(&idle_list))
     {
         kernel_printf("idle list is not empty\n");
         next=get_first_task(&idle_list);
         lower_priority=IDLE_PRIORITY;
         timeslices=IDLE_TIMESLICES;
-        lower_list=&idle_list;
-        list_pcb_add_tail(current,&idle_list);
+        next_list=&idle_list;
     }else 
     {
         kernel_printf("No process foreground\n");
         flag=0;
         counter=BACKGROUND_TIMESLICES;
+        //current->pcb->counter=BACKGROUND_PER_TIMESLICES;
         return 1;
     }
 
+    // old=current;
+    // current=next;
+    // current->pcb->state=STATE_RUNNING;
+    // old->pcb->state=STATE_READY;
+    // old->pcb->priority=lower_priority;
+    // old->pcb->counter=timeslices;
+
     old=current;
     current=next;
-    current->pcb->state=STATE_RUNNING;
     old->pcb->state=STATE_READY;
-    old->pcb->priority=lower_priority;
-    old->pcb->counter=timeslices;
-    //list_pcb_add_tail(old,lower_list);
-    //init_pcb_list(current);
+    current->pcb->counter=timeslices;
+    current->pcb->state=STATE_RUNNING;
+    current->pcb->priority=lower_priority;
 
 
     kernel_printf("              current process name is                 %s\n",current->pcb->name);
@@ -398,24 +508,24 @@ unsigned int sched()
         kernel_printf("state timeslices used up!\n");
         if(flag)
         {
+            //要将current进程放入到合适的前台队列中,next_list全局变量在前台调度函数中进行修改
+            list_pcb_add_tail(current,next_list);
             flag=0;
-
-            kernel_printf("change to %d state\n",flag);
-
             counter=BACKGROUND_TIMESLICES;
+            kernel_printf("change to %d state\n",flag);
             //执行后台调度
-            //index=background_sched();
             if(background_sched())
                 goto sched_error;
         }
         else
         {
+            //将current进程放入到后台队列
+            add_to_background_list(current);
             flag=1;
-
+            counter=FOREGROUNG_TIMESLICES;
             kernel_printf("change to %d state\n",flag);
 
-            counter=FOREGROUNG_TIMESLICES;
-            //取出前台队列中优先级最高队列的第一个进程
+            //取出前台队列中优先级最高队列的第一个进程作为current
             if(foreground_sched())
                 goto sched_error;
         }
@@ -423,7 +533,7 @@ unsigned int sched()
     }
     else
     {
-        kernel_printf("state timeslice has not used up\n");
+        kernel_printf("state timeslice has not used up,rest %d\n",counter);
         if(--(current->pcb->counter))
         {
             kernel_printf("task timeslice has not used up,rest %d\n",current->pcb->counter);
@@ -435,13 +545,17 @@ unsigned int sched()
             kernel_printf("task timeslice has used up\n");
             if(flag)
             {
+                //将current放入到适当的前台队列中
+                list_pcb_add_tail(current,next_list);
                 if(foreground_sched())
                 goto sched_error;
             }
             else
             {
+                //将current放入到后台队列中
+                add_to_background_list(current);
                 if(background_sched())
-                goto sched_error;
+                    goto sched_error;
             }
         }
     }
