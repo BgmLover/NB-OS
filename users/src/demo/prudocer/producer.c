48a8ed4c63f9cee13ea515db1ab7e611b55c6e52
BgmLover <787782453@qq.com>
2018-01-13 13:03:37 +0800

    Revert "Merge branch 'master' of https://github.com/BgmLover/NB_OS"
    This reverts commit 373599f23992760e05e500c33ca044492192efc0, reversing
    changes made to 6b90a128f1959871058c6bdb83205217b38b6458.

---
+++ users/src/demo/prudocer/producer.c
@@ -0,0 +1,35 @@
+#include "producer.h"
+
+void producer(){
+    /*
+    char data;
+    unsigned int offset = 0;
+    struct shared_memory* shm;
+    
+    uprintf("%d\n", 4321);
+    
+    
+	shm = c_shm_test();
+    uprintf("%x\n", (unsigned int)shm);
+    //  while(1);
+
+    // write 26 letters
+    data='a';
+    offset=0;
+    uprintf("offset:%x\n",offset);
+    while(1){
+        uprintf("offset:%x\n",offset);
+        uprintf("write:%c\n",data);
+        offset++;
+        uprintf("offset:%x\n",offset);
+        while(1);
+    }
+
+    while(1){
+
+    }
+    */
+    // uputs("This is a user program.\n",0xf0,0);
+    uprintf("This is a user program.\n");
+    
+}
